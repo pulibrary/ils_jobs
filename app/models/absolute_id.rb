0a2fddf165e2cff1f213b2f6138d8270e1ff53c3
James R. Griffin III <jrgriffiniii@gmail.com>
2021-03-26 16:49:09 -0400

    Restructuring the ContainerProfile parsing for the AbsoluteId model

--- app/models/absolute_id.rb
+++ app/models/absolute_id.rb
@@ -12,14 +12,28 @@ class AbsoluteId < ApplicationRecord
         absolute_id.errors.add(:value, "Mismatch between the digit sequence and the ID") if absolute_id.integer.to_i != absolute_id.barcode.integer
       end
 
-      return unless absolute_id.check_digit.nil?
+      barcode = AbsoluteIds::Barcode.build(absolute_id.value)
+      return if barcode.check_digit == absolute_id.check_digit
 
       absolute_id.errors.add(:check_digit, "Please specify a ID with valid check digit using the Luhn algorithm (please see: https://github.com/topics/luhn-algorithm?l=ruby)")
     end
   end
 
+  class LocatorValidator < ActiveModel::Validator
+    def validate(absolute_id)
+      return if absolute_id.index.nil?
+
+      persisted = AbsoluteId.find_by(index: absolute_id.index, container_profile: absolute_id.container_profile, location: absolute_id.location)
+      return if persisted.nil? || persisted.id == absolute_id.id
+
+      absolute_id.errors.add(:index, "Duplicate index #{absolute_id.index} for the AbID within the Location #{absolute_id.location} and ContainerProfile #{absolute_id.container_profile}")
+    end
+  end
+
   validates :value, presence: true
+  validates :check_digit, presence: true
   validates_with BarcodeValidator
+  validates_with LocatorValidator
 
   belongs_to :batch, class_name: 'AbsoluteId::Batch', optional: true, foreign_key: "absolute_id_batch_id"
 
@@ -51,37 +65,23 @@ class AbsoluteId < ApplicationRecord
     create(**model_attributes)
   end
 
-  def self.prefixes
-    {
-      'Objects' => 'C',
+  def self.sizes
+    LibJobs.config["sizes"]
+  end
 
-      'BoxQ' => 'L',
-      'Double Elephant size box' => 'Z',
-      'Double Elephant volume' => 'D',
-      'Elephant size box' => 'P',
-      'Elephant volume' => 'E',
-      'Folio' => 'F',
+  def self.global_prefixes
+    sizes["global"]
+  end
 
-      'Mudd OS depth' => 'DO',
-      'Mudd OS height' => 'H',
-      'Mudd OS length' => 'LO',
-      'Mudd ST records center' => 'S',
-      'Mudd ST manuscript' => 'S',
-      'Mudd ST half-manuscript' => 'S',
-      'Mudd ST other' => 'S',
-      'Mudd OS open' => 'O',
-
-      'NBox' => 'B',
-      'Ordinary' => 'N',
-      'Quarto' => 'Q',
-      'Small' => 'S'
-    }
+  def self.local_prefixes
+    sizes.select { |k, v| k != "global" && v.is_a?(Hash) }
   end
 
   def self.find_prefix(key)
-    return unless prefixes.key?(key)
+    local_merged = local_prefixes.to_h.values.inject(:merge)
+    merged = global_prefixes.merge(local_merged)
 
-    prefixes[key]
+    merged[key]
   end
 
   def self.find_prefixed_models(prefix:)
@@ -100,21 +100,45 @@ class AbsoluteId < ApplicationRecord
   end
   delegate :digits, :elements, to: :barcode
 
+  def find_local_prefixes(key)
+    self.class.local_prefixes[key]
+  end
+
+  def local_prefixes
+    @local_prefixes ||= begin
+                          if location_object.name
+                            find_local_prefixes(location.key)
+                          elsif self.class.local_prefixes.key?(location)
+                            find_local_prefixes(location)
+                          else
+                            {}
+                          end
+                        end
+  end
+
+  def prefixes
+    @prefixes ||= begin
+                    self.class.global_prefixes.merge(local_prefixes)
+                  end
+  end
+
   def size
     if container_profile_object.name
-      self.class.find_prefix(container_profile_object.name)
-    elsif self.class.prefixes.key?(container_profile)
-      self.class.find_prefix(container_profile)
+      prefixes[container_profile_object.name]
+    elsif prefixes.key?(container_profile)
+      prefixes[container_profile]
     else
       container_profile
     end
   end
+  alias prefix size
 
-  def label
+  def locator
     return if index.nil? || size.nil?
 
     format("%s-%06d", size, index)
   end
+  alias label locator
 
   # For ASpace Locations
   def location_object
